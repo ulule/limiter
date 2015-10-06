b778d08a852e3a7cd4abfd062c8fe20f44c7757b
Gilles Fabio <gilles@gillesfabio.com>
2015-10-06 17:05:00 +0200

    Add middleware tests to tests race.

--- middleware_http_test.go
+++ middleware_http_test.go
@@ -3,6 +3,8 @@ package limiter
 import (
 	"net/http"
 	"net/http/httptest"
+	"runtime"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,4 +32,39 @@ func TestHTTPMiddleware(t *testing.T) {
 		}
 		i++
 	}
+}
+
+// TestHTTPMiddlewareWithRaceCondition tests the HTTP middleware under race condition.
+func TestHTTPMiddlewareWithRaceCondition(t *testing.T) {
+	runtime.GOMAXPROCS(4)
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.RemoteAddr = "178.1.2.28:128"
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
+
+	mw := NewHTTPMiddleware(newRedisLimiter("11-M", "limitertests:http")).Handler(handler)
+
+	nbRequests := 100
+	successCount := 0
+
+	var wg sync.WaitGroup
+	wg.Add(nbRequests)
+
+	for i := 1; i <= nbRequests; i++ {
+		go func() {
+			res := httptest.NewRecorder()
+			mw.ServeHTTP(res, req)
+			if res.Code == 200 {
+				successCount++
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, 11, successCount)
 }
