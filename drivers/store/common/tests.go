e19dcc5134b3a353da1bc0aed1ddecb10d4c59c6
Thomas LE ROUX <thomas@leroux.io>
2017-08-17 15:38:03 +0200

    feat(store): add common test for store

---
+++ drivers/store/common/tests.go
@@ -0,0 +1,81 @@
+package common
+
+import (
+	"context"
+	"math"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ulule/limiter"
+)
+
+func TestStoreSequentialAccess(t *testing.T, store limiter.Store) {
+	is := require.New(t)
+	ctx := context.Background()
+
+	limiter := limiter.New(store, limiter.Rate{
+		Limit:  3,
+		Period: time.Minute,
+	})
+
+	for i := 1; i <= 6; i++ {
+
+		if i <= 3 {
+
+			lctx, err := limiter.Peek(ctx, "boo")
+			is.NoError(err)
+			is.NotZero(lctx)
+			is.Equal(int64(3-(i-1)), lctx.Remaining)
+
+		}
+
+		lctx, err := limiter.Get(ctx, "boo")
+		is.NoError(err)
+		is.NotZero(lctx)
+
+		if i <= 3 {
+
+			is.Equal(int64(3), lctx.Limit)
+			is.Equal(int64(3-i), lctx.Remaining)
+			is.True(math.Ceil(time.Since(time.Unix(lctx.Reset, 0)).Seconds()) <= 60)
+
+			lctx, err := limiter.Peek(ctx, "boo")
+			is.NoError(err)
+			is.Equal(int64(3-i), lctx.Remaining)
+
+		} else {
+
+			is.Equal(int64(3), lctx.Limit)
+			is.True(lctx.Remaining == 0)
+			is.True(math.Ceil(time.Since(time.Unix(lctx.Reset, 0)).Seconds()) <= 60)
+
+		}
+	}
+}
+
+func TestStoreConcurrentAccess(t *testing.T, store limiter.Store) {
+	is := require.New(t)
+	ctx := context.Background()
+
+	limiter := limiter.New(store, limiter.Rate{
+		Limit:  100000,
+		Period: 5 * time.Nanosecond,
+	})
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 8000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			for j := 0; j < 1000; j++ {
+				lctx, err := limiter.Get(ctx, "boo")
+				is.NoError(err)
+				is.NotZero(lctx)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
