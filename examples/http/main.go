8908d6ae8bda5b4581a59039a7a1e1f219822741
Thomas LE ROUX <thomas@leroux.io>
2017-08-18 14:32:10 +0200

    docs: update examples

--- examples/http/main.go
+++ examples/http/main.go
@@ -5,44 +5,50 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/garyburd/redigo/redis"
+	libredis "github.com/go-redis/redis"
 	"github.com/ulule/limiter"
+	"github.com/ulule/limiter/drivers/middleware/stdlib"
+	"github.com/ulule/limiter/drivers/store/redis"
 )
 
 func main() {
-	// 4 reqs/hour
+
+	// Define a limit rate to 4 requests per hour.
 	rate, err := limiter.NewRateFromFormatted("4-H")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
-	// Create a Redis pool.
-	pool := redis.NewPool(func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", ":6379")
-		if err != nil {
-			return nil, err
-		}
-		return c, err
-	}, 100)
-
-	// Create a store with the pool.
-	store, err := limiter.NewRedisStoreWithOptions(
-		pool,
-		limiter.StoreOptions{Prefix: "limiter_http_example", MaxRetry: 3})
-
+	// Create a redis client.
+	option, err := libredis.ParseURL("redis://localhost:6379/0")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
+	}
+	client := libredis.NewClient(option)
+
+	// Create a store with the redis client.
+	store, err := redis.NewStoreWithOptions(client, limiter.StoreOptions{
+		Prefix:   "limiter_http_example",
+		MaxRetry: 3,
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
-	mw := limiter.NewHTTPMiddleware(limiter.NewLimiter(store, rate))
-	http.Handle("/", mw.Handler(http.HandlerFunc(index)))
+	// Create a new middleware with the limiter instance.
+	middleware := stdlib.NewMiddleware(limiter.New(store, rate))
 
+	// Launch a simple server.
+	http.Handle("/", middleware.Handler(http.HandlerFunc(index)))
 	fmt.Println("Server is running on port 7777...")
 	log.Fatal(http.ListenAndServe(":7777", nil))
 
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Write([]byte(`{"message": "ok"}`))
 }
