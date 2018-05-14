f17a702432c6cdef5f22c7faa8e9c20a7d1b44a8
Thomas LE ROUX <thomas@november-eleven.fr>
2018-05-14 21:50:04 +0200

    ref(drivers): Move testing helpers on a separated package

--- drivers/store/memory/store_test.go
+++ drivers/store/memory/store_test.go
@@ -5,19 +5,19 @@ import (
 	"time"
 
 	"github.com/ulule/limiter"
-	"github.com/ulule/limiter/drivers/store/common"
 	"github.com/ulule/limiter/drivers/store/memory"
+	"github.com/ulule/limiter/drivers/store/tests"
 )
 
 func TestMemoryStoreSequentialAccess(t *testing.T) {
-	common.TestStoreSequentialAccess(t, memory.NewStoreWithOptions(limiter.StoreOptions{
+	tests.TestStoreSequentialAccess(t, memory.NewStoreWithOptions(limiter.StoreOptions{
 		Prefix:          "limiter:memory:sequential",
 		CleanUpInterval: 30 * time.Second,
 	}))
 }
 
 func TestMemoryStoreConcurrentAccess(t *testing.T) {
-	common.TestStoreConcurrentAccess(t, memory.NewStoreWithOptions(limiter.StoreOptions{
+	tests.TestStoreConcurrentAccess(t, memory.NewStoreWithOptions(limiter.StoreOptions{
 		Prefix:          "limiter:memory:concurrent",
 		CleanUpInterval: 1 * time.Nanosecond,
 	}))
--- drivers/store/tests/tests.go
+++ drivers/store/tests/tests.go
@@ -1,4 +1,4 @@
-package common
+package tests
 
 import (
 	"context"
