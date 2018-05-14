af07f8759cbc68c744a15ffbfdabf35f7aff5e3b
Thomas LE ROUX <thomas@november-eleven.fr>
2018-05-14 21:57:53 +0200

    chore: reduce goroutine stress test

--- drivers/store/memory/cache_test.go
+++ drivers/store/memory/cache_test.go
@@ -38,7 +38,7 @@ func TestCacheIncrementSequential(t *testing.T) {
 func TestCacheIncrementConcurrent(t *testing.T) {
 	is := require.New(t)
 
-	goroutines := 500
+	goroutines := 300
 	ops := 500
 
 	expected := int64(0)
