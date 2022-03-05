f44a878e9da911ad0780c56c3dad1df6799ae6dd
Thomas LE ROUX <thomas@leroux.io>
2022-03-05 14:16:59 +0100

    fix(security): Add strong warnings about XFF headers

--- network.go
+++ network.go
@@ -14,35 +14,59 @@ var (
 )
 
 // GetIP returns IP address from request.
+// If options is defined and either TrustForwardHeader is true or ClientIPHeader is defined,
+// it will lookup IP in HTTP headers.
+// Please be advised that using this option could be insecure (ie: spoofed) if your reverse
+// proxy is not configured properly to forward a trustworthy client IP.
+// Please read the section "Limiter behind a reverse proxy" in the README for further information.
 func (limiter *Limiter) GetIP(r *http.Request) net.IP {
 	return GetIP(r, limiter.Options)
 }
 
 // GetIPWithMask returns IP address from request by applying a mask.
+// If options is defined and either TrustForwardHeader is true or ClientIPHeader is defined,
+// it will lookup IP in HTTP headers.
+// Please be advised that using this option could be insecure (ie: spoofed) if your reverse
+// proxy is not configured properly to forward a trustworthy client IP.
+// Please read the section "Limiter behind a reverse proxy" in the README for further information.
 func (limiter *Limiter) GetIPWithMask(r *http.Request) net.IP {
 	return GetIPWithMask(r, limiter.Options)
 }
 
 // GetIPKey extracts IP from request and returns hashed IP to use as store key.
+// If options is defined and either TrustForwardHeader is true or ClientIPHeader is defined,
+// it will lookup IP in HTTP headers.
+// Please be advised that using this option could be insecure (ie: spoofed) if your reverse
+// proxy is not configured properly to forward a trustworthy client IP.
+// Please read the section "Limiter behind a reverse proxy" in the README for further information.
 func (limiter *Limiter) GetIPKey(r *http.Request) string {
 	return limiter.GetIPWithMask(r).String()
 }
 
 // GetIP returns IP address from request.
-// If options is defined and TrustForwardHeader is true, it will lookup IP in
-// X-Forwarded-For and X-Real-IP headers.
+// If options is defined and either TrustForwardHeader is true or ClientIPHeader is defined,
+// it will lookup IP in HTTP headers.
+// Please be advised that using this option could be insecure (ie: spoofed) if your reverse
+// proxy is not configured properly to forward a trustworthy client IP.
+// Please read the section "Limiter behind a reverse proxy" in the README for further information.
 func GetIP(r *http.Request, options ...Options) net.IP {
-	if len(options) >= 1 && options[0].TrustForwardHeader {
-		ip := r.Header.Get("X-Forwarded-For")
-		if ip != "" {
-			parts := strings.SplitN(ip, ",", 2)
-			part := strings.TrimSpace(parts[0])
-			return net.ParseIP(part)
+	if len(options) >= 1 {
+		if options[0].ClientIPHeader != "" {
+			ip := getIPFromHeader(r, options[0].ClientIPHeader)
+			if ip != nil {
+				return ip
+			}
 		}
+		if options[0].TrustForwardHeader {
+			ip := getIPFromXFFHeader(r)
+			if ip != nil {
+				return ip
+			}
 
-		ip = strings.TrimSpace(r.Header.Get("X-Real-IP"))
-		if ip != "" {
-			return net.ParseIP(ip)
+			ip = getIPFromHeader(r, "X-Real-IP")
+			if ip != nil {
+				return ip
+			}
 		}
 	}
 
@@ -56,6 +80,11 @@ func GetIP(r *http.Request, options ...Options) net.IP {
 }
 
 // GetIPWithMask returns IP address from request by applying a mask.
+// If options is defined and either TrustForwardHeader is true or ClientIPHeader is defined,
+// it will lookup IP in HTTP headers.
+// Please be advised that using this option could be insecure (ie: spoofed) if your reverse
+// proxy is not configured properly to forward a trustworthy client IP.
+// Please read the section "Limiter behind a reverse proxy" in the README for further information.
 func GetIPWithMask(r *http.Request, options ...Options) net.IP {
 	if len(options) == 0 {
 		return GetIP(r)
@@ -69,4 +98,40 @@ func GetIPWithMask(r *http.Request, options ...Options) net.IP {
 		return ip.Mask(options[0].IPv6Mask)
 	}
 	return ip
+}
+
+func getIPFromXFFHeader(r *http.Request) net.IP {
+	headers := r.Header.Values("X-Forwarded-For")
+	if len(headers) == 0 {
+		return nil
+	}
+
+	parts := []string{}
+	for _, header := range headers {
+		parts = append(parts, strings.Split(header, ",")...)
+	}
+
+	for i := range parts {
+		part := strings.TrimSpace(parts[i])
+		ip := net.ParseIP(part)
+		if ip != nil {
+			return ip
+		}
+	}
+
+	return nil
+}
+
+func getIPFromHeader(r *http.Request, name string) net.IP {
+	header := strings.TrimSpace(r.Header.Get(name))
+	if header == "" {
+		return nil
+	}
+
+	ip := net.ParseIP(header)
+	if ip != nil {
+		return ip
+	}
+
+	return nil
 }
