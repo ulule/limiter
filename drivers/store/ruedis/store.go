ada4057a2caa5b9e6609604dd989bc3538ca9b6e
rusdevops <rusdevops@gmail.com>
2024-04-29 13:34:03 +0300

    added ruedis driver

---
+++ drivers/store/ruedis/store.go
@@ -0,0 +1,269 @@
+package redis
+
+import (
+	"context"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/pkg/errors"
+	libredis "github.com/redis/rueidis/rueidiscompat"
+
+	"github.com/ulule/limiter/v3"
+	"github.com/ulule/limiter/v3/drivers/store/common"
+)
+
+const (
+	luaIncrScript = `
+local key = KEYS[1]
+local count = tonumber(ARGV[1])
+local ttl = tonumber(ARGV[2])
+local ret = redis.call("incrby", key, ARGV[1])
+if ret == count then
+	if ttl > 0 then
+		redis.call("pexpire", key, ARGV[2])
+	end
+	return {ret, ttl}
+end
+ttl = redis.call("pttl", key)
+return {ret, ttl}
+`
+	luaPeekScript = `
+local key = KEYS[1]
+local v = redis.call("get", key)
+if v == false then
+	return {0, 0}
+end
+local ttl = redis.call("pttl", key)
+return {tonumber(v), ttl}
+`
+)
+
+// Client is an interface thats allows to use a redis cluster or a redis single client seamlessly.
+type Client interface {
+	Get(ctx context.Context, key string) *libredis.StringCmd
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *libredis.StatusCmd
+	Del(ctx context.Context, keys ...string) *libredis.IntCmd
+	SetNX(ctx context.Context, key string, value interface{}, expiration time.Duration) *libredis.BoolCmd
+	EvalSha(ctx context.Context, sha string, keys []string, args ...interface{}) *libredis.Cmd
+	ScriptLoad(ctx context.Context, script string) *libredis.StringCmd
+}
+
+// Store is the redis store.
+type Store struct {
+	// Prefix used for the key.
+	Prefix string
+	// MaxRetry is the maximum number of retry under race conditions.
+	// Deprecated: this option is no longer required since all operations are atomic now.
+	MaxRetry int
+	// client used to communicate with redis server.
+	client Client
+	// luaMutex is a mutex used to avoid concurrent access on luaIncrSHA and luaPeekSHA.
+	luaMutex sync.RWMutex
+	// luaLoaded is used for CAS and reduce pressure on luaMutex.
+	luaLoaded uint32
+	// luaIncrSHA is the SHA of increase and expire key script.
+	luaIncrSHA string
+	// luaPeekSHA is the SHA of peek and expire key script.
+	luaPeekSHA string
+}
+
+// NewStore returns an instance of redis store with defaults.
+func NewStore(client Client) (limiter.Store, error) {
+	return NewStoreWithOptions(client, limiter.StoreOptions{
+		Prefix:          limiter.DefaultPrefix,
+		CleanUpInterval: limiter.DefaultCleanUpInterval,
+		MaxRetry:        limiter.DefaultMaxRetry,
+	})
+}
+
+// NewStoreWithOptions returns an instance of redis store with options.
+func NewStoreWithOptions(client Client, options limiter.StoreOptions) (limiter.Store, error) {
+	store := &Store{
+		client:   client,
+		Prefix:   options.Prefix,
+		MaxRetry: options.MaxRetry,
+	}
+
+	err := store.preloadLuaScripts(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return store, nil
+}
+
+// Increment increments the limit by given count & gives back the new limit for given identifier
+func (store *Store) Increment(ctx context.Context, key string, count int64, rate limiter.Rate) (limiter.Context, error) {
+	cmd := store.evalSHA(ctx, store.getLuaIncrSHA, []string{store.getCacheKey(key)}, count, rate.Period.Milliseconds())
+	return currentContext(cmd, rate)
+}
+
+// Get returns the limit for given identifier.
+func (store *Store) Get(ctx context.Context, key string, rate limiter.Rate) (limiter.Context, error) {
+	cmd := store.evalSHA(ctx, store.getLuaIncrSHA, []string{store.getCacheKey(key)}, 1, rate.Period.Milliseconds())
+	return currentContext(cmd, rate)
+}
+
+// Peek returns the limit for given identifier, without modification on current values.
+func (store *Store) Peek(ctx context.Context, key string, rate limiter.Rate) (limiter.Context, error) {
+	cmd := store.evalSHA(ctx, store.getLuaPeekSHA, []string{store.getCacheKey(key)})
+	count, ttl, err := parseCountAndTTL(cmd)
+	if err != nil {
+		return limiter.Context{}, err
+	}
+
+	now := time.Now()
+	expiration := now.Add(rate.Period)
+	if ttl > 0 {
+		expiration = now.Add(time.Duration(ttl) * time.Millisecond)
+	}
+
+	return common.GetContextFromState(now, rate, expiration, count), nil
+}
+
+// Reset returns the limit for given identifier which is set to zero.
+func (store *Store) Reset(ctx context.Context, key string, rate limiter.Rate) (limiter.Context, error) {
+	_, err := store.client.Del(ctx, store.getCacheKey(key)).Result()
+	if err != nil {
+		return limiter.Context{}, err
+	}
+
+	count := int64(0)
+	now := time.Now()
+	expiration := now.Add(rate.Period)
+
+	return common.GetContextFromState(now, rate, expiration, count), nil
+}
+
+// getCacheKey returns the full path for an identifier.
+func (store *Store) getCacheKey(key string) string {
+	buffer := strings.Builder{}
+	buffer.WriteString(store.Prefix)
+	buffer.WriteString(":")
+	buffer.WriteString(key)
+	return buffer.String()
+}
+
+// preloadLuaScripts preloads the "incr" and "peek" lua scripts.
+func (store *Store) preloadLuaScripts(ctx context.Context) error {
+	// Verify if we need to load lua scripts.
+	// Inspired by sync.Once.
+	if atomic.LoadUint32(&store.luaLoaded) == 0 {
+		return store.loadLuaScripts(ctx)
+	}
+	return nil
+}
+
+// reloadLuaScripts forces a reload of "incr" and "peek" lua scripts.
+func (store *Store) reloadLuaScripts(ctx context.Context) error {
+	// Reset lua scripts loaded state.
+	// Inspired by sync.Once.
+	atomic.StoreUint32(&store.luaLoaded, 0)
+	return store.loadLuaScripts(ctx)
+}
+
+// loadLuaScripts load "incr" and "peek" lua scripts.
+// WARNING: Please use preloadLuaScripts or reloadLuaScripts, instead of this one.
+func (store *Store) loadLuaScripts(ctx context.Context) error {
+	store.luaMutex.Lock()
+	defer store.luaMutex.Unlock()
+
+	// Check if scripts are already loaded.
+	if atomic.LoadUint32(&store.luaLoaded) != 0 {
+		return nil
+	}
+
+	luaIncrSHA, err := store.client.ScriptLoad(ctx, luaIncrScript).Result()
+	if err != nil {
+		return errors.Wrap(err, `failed to load "incr" lua script`)
+	}
+
+	luaPeekSHA, err := store.client.ScriptLoad(ctx, luaPeekScript).Result()
+	if err != nil {
+		return errors.Wrap(err, `failed to load "peek" lua script`)
+	}
+
+	store.luaIncrSHA = luaIncrSHA
+	store.luaPeekSHA = luaPeekSHA
+
+	atomic.StoreUint32(&store.luaLoaded, 1)
+
+	return nil
+}
+
+// getLuaIncrSHA returns a "thread-safe" value for luaIncrSHA.
+func (store *Store) getLuaIncrSHA() string {
+	store.luaMutex.RLock()
+	defer store.luaMutex.RUnlock()
+	return store.luaIncrSHA
+}
+
+// getLuaPeekSHA returns a "thread-safe" value for luaPeekSHA.
+func (store *Store) getLuaPeekSHA() string {
+	store.luaMutex.RLock()
+	defer store.luaMutex.RUnlock()
+	return store.luaPeekSHA
+}
+
+// evalSHA eval the redis lua sha and load the scripts if missing.
+func (store *Store) evalSHA(ctx context.Context, getSha func() string,
+	keys []string, args ...interface{}) *libredis.Cmd {
+
+	cmd := store.client.EvalSha(ctx, getSha(), keys, args...)
+	err := cmd.Err()
+	if err == nil || !isLuaScriptGone(err) {
+		return cmd
+	}
+
+	err = store.reloadLuaScripts(ctx)
+	if err != nil {
+		cmd = &libredis.Cmd{}
+		cmd.SetErr(err)
+		return cmd
+	}
+
+	return store.client.EvalSha(ctx, getSha(), keys, args...)
+}
+
+// isLuaScriptGone returns if the error is a missing lua script from redis server.
+func isLuaScriptGone(err error) bool {
+	return strings.HasPrefix(err.Error(), "NOSCRIPT")
+}
+
+// parseCountAndTTL parse count and ttl from lua script output.
+func parseCountAndTTL(cmd *libredis.Cmd) (int64, int64, error) {
+	result, err := cmd.Result()
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "an error has occurred with redis command")
+	}
+
+	fields, ok := result.([]interface{})
+	if !ok || len(fields) != 2 {
+		return 0, 0, errors.New("two elements in result were expected")
+	}
+
+	count, ok1 := fields[0].(int64)
+	ttl, ok2 := fields[1].(int64)
+	if !ok1 || !ok2 {
+		return 0, 0, errors.New("type of the count and/or ttl should be number")
+	}
+
+	return count, ttl, nil
+}
+
+func currentContext(cmd *libredis.Cmd, rate limiter.Rate) (limiter.Context, error) {
+	count, ttl, err := parseCountAndTTL(cmd)
+	if err != nil {
+		return limiter.Context{}, err
+	}
+
+	now := time.Now()
+	expiration := now.Add(rate.Period)
+	if ttl > 0 {
+		expiration = now.Add(time.Duration(ttl) * time.Millisecond)
+	}
+
+	return common.GetContextFromState(now, rate, expiration, count), nil
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/pkg/errors v0.9.1
 	github.com/redis/go-redis/v9 v9.0.5
+	github.com/redis/rueidis/rueidiscompat v1.0.35
 	github.com/stretchr/testify v1.8.4
 	github.com/valyala/fasthttp v1.47.0
 )
@@ -23,7 +24,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -33,15 +33,15 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/redis/rueidis v1.0.35 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
